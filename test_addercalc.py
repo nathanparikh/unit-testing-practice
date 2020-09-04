e6be529ba57c8a6b9f7609fef567ff920d6a30aa
Nathan Parikh <nathan.parikh@duke.edu>
2020-09-04 16:10:38 -0700

    Created addercalc module to perform line calculations and return proper y value

--- test_addercalc.py
+++ test_addercalc.py
@@ -1,23 +1,14 @@
 import pytest
 
-@pytest.mark.parametrize ("(x1,y1)", [
-	(0,0),
-	(3,4),
-	])
-@pytest.mark.parametrize ("(x2,y2)", [
-	(1,1),
-	(6,8),
-	])
-@pytest.mark.parametrize ("x", [
-	2,
-	9,
-	])
-@pytest.mark.parametrize ("expected", [
-	2,
-	12,
+@pytest.mark.parametrize ("x1,y1,x2,y2,x,expected", [
+	(0,0,1,1,2,2),
+	(4,8,12,24,8,16),
+	(0,0,5,0,7,0),
+	(4,6,6,8,7,9),
+	(1,3,1,6,0,"error"),
 	])
 
-def test_adder ((x1,y1),(x2,y2),x,expected):
+def test_adder (x1,y1,x2,y2,x,expected):
 	from addercalc import adder
-	result = adder((x1,x2),(x2,y2),x)
+	result = adder(x1,y1,x2,y2,x)
 	assert result == expected
